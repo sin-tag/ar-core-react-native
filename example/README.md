1588c41b0ce29d9cc6e149e3a2ae12f9a9a1dab0
tuyenvq <tuyenvq@d2t.vn>
2021-05-11 15:12:53 +0700

    add example

---
+++ example/README.md
@@ -0,0 +1,13 @@
+# Example for ARCore and ARKit for react-native
+```sh
+# clone project
+git clone https://github.com/sin-tag/ar-core-react-native
+cd ar-core-react-native/example
+npm install
+# android
+npx react-native run-android
+# ios
+cd ios
+pod install
+# open xcode 
+```
--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,14 +1,26 @@
 import * as React from 'react';
 
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View, TouchableOpacity, UIManager, findNodeHandle, Text } from 'react-native';
 import ArCoreReactNativeViewManager from 'ar-core-react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <ArCoreReactNativeViewManager color="yellow" style={styles.box} />
-    </View>
-  );
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+
+        <ArCoreReactNativeViewManager color="yellow" style={styles.box} ref="arCoreView" />
+        <TouchableOpacity onPress={() => {
+          UIManager.dispatchViewManagerCommand(
+            findNodeHandle(this.refs.arCoreView),
+            "CMD_RUN_SET_OBJECT",
+            ["001", ""]);
+        }} style={styles.buttonAction}>
+          <Text>Add Object</Text>
+        </TouchableOpacity>
+      </View>
+    )
+  }
+
 }
 
 const styles = StyleSheet.create({
@@ -22,4 +34,9 @@ const styles = StyleSheet.create({
     height: '100%',
     // marginVertical: 20,
   },
+  buttonAction: {
+    position: 'absolute', 
+    top: 30, left: 0, right: 0, 
+    justifyContent: 'center', alignItems: 'center'
+  }
 });
